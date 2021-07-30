3a077979cae79a5ac1f5fdecfedaf38c990c4fa2
kangjihye <kjh9519@naver.com>
2021-07-30 19:54:23 +0900

    Revert "복구"
    This reverts commit b6c96cabd5d07d0cb0a049550ac1325b3af2b70f.

--- flosum_project/flosum_project/templates/js/button.js
+++
@@ -1,7 +0,0 @@
-function changeBunchSize(targetId) {
-  console.log(targetId);
-  ['S', 'M', 'L'].forEach((v) => {
-    if (v === targetId) document.getElementById(v).classList.add('active');
-    else document.getElementById(v).classList.remove('active');
-  });
-}
