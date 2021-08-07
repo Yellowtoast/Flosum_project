be18ee9776355bef72e198777d4170cac06b30d7
rudan <ruddd8256@naver.com>
2021-08-07 15:59:40 +0900

    전체삭제

--- flosum_project/customizingapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Flower, Decoration, Bouquet, Wrapper
-
-admin.site.register(Flower)
-admin.site.register(Decoration)
-admin.site.register(Bouquet)
-admin.site.register(Wrapper)
--- flosum_project/customizingapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomizingappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'customizingapp'
